0ab0d3988d20cea8b8767cabe60e6c76a6e04e89
Tpoveda02 <taniazoelpoveda@gmail.com>
2024-12-24 17:08:17 -0500

    feat: Create basic models
    Room.java Accomodation.java; Client.java; Reservation.java

---
+++ src/main/java/org/example/model/Accommodation.java
@@ -0,0 +1,13 @@
+package org.example.model;
+
+import java.util.List;
+
+public class Accommodation {
+
+    private String name;
+    private String city;
+    private double rating;
+    private double pricePerNight;
+    private List<Room> rooms;
+
+}
---
+++ src/main/java/org/example/model/Room.java
@@ -0,0 +1,9 @@
+package org.example.model;
+
+public class Room {
+    private String type;
+    private double price;
+    private String features;
+    private int quantityAvailable;
+
+}
