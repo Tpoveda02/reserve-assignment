5182f7b3e6054433ff9633661fba65222193895e
Tpoveda02 <taniazoelpoveda@gmail.com>
2024-12-25 11:34:19 -0500

    fix: fix essentials methods changed by lombok

--- src/main/java/org/example/model/Client.java
+++ src/main/java/org/example/model/Client.java
@@ -1,7 +1,15 @@
 package org.example.model;
 
-import java.time.LocalTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.time.LocalTime;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Client {
 
     private String id;
@@ -12,72 +20,5 @@ public class Client {
     private String phoneNumber;
     private LocalTime birthdate;
 
-    //Constructor
-    public Client(String id, String firstName, String lastName, String email, String nationality, String phoneNumber, LocalTime birthdate) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.nationality = nationality;
-        this.phoneNumber = phoneNumber;
-        this.birthdate = birthdate;
-    }
-    //Getters and Setters
 
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getNationality() {
-        return nationality;
-    }
-
-    public void setNationality(String nationality) {
-        this.nationality = nationality;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public LocalTime getBirthdate() {
-        return birthdate;
-    }
-
-    public void setBirthdate(LocalTime birthdate) {
-        this.birthdate = birthdate;
-    }
 }
--- src/main/java/org/example/model/Reservation.java
+++ src/main/java/org/example/model/Reservation.java
@@ -1,8 +1,16 @@
 package org.example.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.time.LocalDate;
 import java.util.List;
-
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Reservation {
     private Client client;
     private Accommodation accommodation;
@@ -15,100 +23,4 @@ public class Reservation {
     private double totalPrice;
     private double discountOrIncrease;
 
-    //Constructor
-
-    public Reservation(Client client, Accommodation accommodation, LocalDate startDate, LocalDate endDate, int adults, int children, int numberOfRooms, List<Room> roomsReserved, double totalPrice, double discountOrIncrease) {
-        this.client = client;
-        this.accommodation = accommodation;
-        this.startDate = startDate;
-        this.endDate = endDate;
-        this.adults = adults;
-        this.children = children;
-        this.numberOfRooms = numberOfRooms;
-        this.roomsReserved = roomsReserved;
-        this.totalPrice = totalPrice;
-        this.discountOrIncrease = discountOrIncrease;
-    }
-
-    //Getters and Setters
-
-    public Client getClient() {
-        return client;
-    }
-
-    public void setClient(Client client) {
-        this.client = client;
-    }
-
-    public Accommodation getAccommodation() {
-        return accommodation;
-    }
-
-    public void setAccommodation(Accommodation accommodation) {
-        this.accommodation = accommodation;
-    }
-
-    public LocalDate getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(LocalDate startDate) {
-        this.startDate = startDate;
-    }
-
-    public LocalDate getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(LocalDate endDate) {
-        this.endDate = endDate;
-    }
-
-    public int getAdults() {
-        return adults;
-    }
-
-    public void setAdults(int adults) {
-        this.adults = adults;
-    }
-
-    public int getChildren() {
-        return children;
-    }
-
-    public void setChildren(int children) {
-        this.children = children;
-    }
-
-    public int getNumberOfRooms() {
-        return numberOfRooms;
-    }
-
-    public void setNumberOfRooms(int numberOfRooms) {
-        this.numberOfRooms = numberOfRooms;
-    }
-
-    public List<Room> getRoomsReserved() {
-        return roomsReserved;
-    }
-
-    public void setRoomsReserved(List<Room> roomsReserved) {
-        this.roomsReserved = roomsReserved;
-    }
-
-    public double getTotalPrice() {
-        return totalPrice;
-    }
-
-    public void setTotalPrice(double totalPrice) {
-        this.totalPrice = totalPrice;
-    }
-
-    public double getDiscountOrIncrease() {
-        return discountOrIncrease;
-    }
-
-    public void setDiscountOrIncrease(double discountOrIncrease) {
-        this.discountOrIncrease = discountOrIncrease;
-    }
 }
